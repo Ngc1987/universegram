76f56e2f7ce04d3576b17cd8bbf2e6e1c43a1900
Thomas Semeria <tsemeri@gmail.com>
2022-05-04 22:29:26 +0200

    Trying correct Port env

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 const corsOptions = {
 	origin: process.env.CLIENT_URL,
@@ -58,7 +59,7 @@ if(process.env.NODE_ENV === "production"){
 }
 
 // Server
-app.listen(process.env.PORT || 5000, () => {
+app.listen(PORT, () => {
 	console.log(`Server is running on ${process.env.PORT
 		}`);
 })
