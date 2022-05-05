e161246651f70f137b23066c6e38621025761c44
Thomas Semeria <tsemeri@gmail.com>
2022-05-05 16:54:45 +0200

    Trying new options to token options to make the cookie works #3

--- client/src/App.js
+++ client/src/App.js
@@ -2,41 +2,44 @@ import React, {useEffect, useState} from "react";
 import Routes from "././components/Routes";
 import { UidContext } from "./components/AppContext";
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "./actions/user.actions";
 
 function App() {
 
 	const [uid, setUid] = useState(null);
 
+	const userToken = useSelector((state) => state.userReducer.token);
 	const dispatch = useDispatch();
 
+	console.log(uid, userToken)
+
 	useEffect(()=> {
-		async function fetchUserToken() {
-			await axios({
-				method: "get",
-				url: `${process.env.REACT_APP_API_URL}/jwtid`,
-				withCredentials: true
-			})
-			.then((res) => {
-				console.log(res)
-				setUid(res.data);
-			})
-			.catch((err) => {
-				console.log("No token")
-			})
+		// async function fetchUserToken() {
+		// 	await axios({
+		// 		method: "get",
+		// 		url: `${process.env.REACT_APP_API_URL}/jwtid`,
+		// 		withCredentials: true
+		// 	})
+		// 	.then((res) => {
+		// 		console.log(res)
+		// 		setUid(res.data);
+		// 	})
+		// 	.catch((err) => {
+		// 		console.log("No token")
+		// 	})
 
-		}
-		fetchUserToken();
+		// }
+		// fetchUserToken();
 
-		if(uid) dispatch(getUser(uid));
+		if(userToken) dispatch(getUser(userToken));
 		
 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [uid])
+	}, [userToken])
 
 	
 	return (
-		<UidContext.Provider value={uid} >
+		<UidContext.Provider value={userToken} >
 			<Routes/>
 		</UidContext.Provider>
 	);
