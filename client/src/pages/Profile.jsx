027edb4877921675cbcd045620d2e96761b1dd8d
Thomas Semeria <tsemeri@gmail.com>
2022-05-07 13:30:02 +0200

    Add post on user profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Log from "../components/Log/Log";
 import { UidContext } from "../components/AppContext";
 import UpdateProfile from '../components/Profile/UpdateProfile';
+import Card from './../components/Posts/Card';
+import { useSelector } from 'react-redux';
 
 
 /**
@@ -9,13 +11,23 @@ import UpdateProfile from '../components/Profile/UpdateProfile';
  * @description It's the Profile page
  */
 const Profile = () => {
-
-
+	
+	const posts = useSelector((state) => state.postReducer);
 	const uid = useContext(UidContext);
+
 	return (
 		<div className="profil-page">
 		{uid ? 
+		<>
 			<UpdateProfile/>
+			{posts.map((post) => {
+				if(post.posterId === uid) {
+					return (
+						<Card post={post} key={post._id} />
+					)
+				} return null
+			})}
+		</>
 			:
 
 			<div className="log-container">
